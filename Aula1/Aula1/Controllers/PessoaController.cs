aa64744a39c824dc83244c05e4d8786f2e8b287f
Gustavo Pirondi <gustavopirondi@gmail.com>
2025-02-04 22:14:23 -0300

    Aula 1 p1
    Primeira parte do conteúdo passado na Aula 1, onde é visto a parte inicial do httpGet

---
+++ Aula1/Aula1/Controllers/PessoaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aula1.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Salvar()
+        {
+            return View();
+        }
+
+        public IActionResult Buscar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Salvar1(String nome, String email)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Salvar2()
+        {
+            return View();
+        }
+    }
+}
